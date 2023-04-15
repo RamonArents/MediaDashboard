be181c4b0c6249677e99291e0a98bd9e35832d7d
ramon arents <ramonarents@hotmail.com>
2023-04-15 18:08:44 +0200

    webpage images almost done

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from itertools import combinations
 
 import cloudinary_service
 
-@st.cache
+@st.cache_data
 def get_images_with_tags():
     return cloudinary_service.get_all_images_with_tags()
 
@@ -24,5 +24,42 @@ for image in all_images:
 
 tag_counter = Counter(all_tags)
 
-print(tag_counter)
-print(all_tags_lists)
+#print(tag_counter)
+#print(all_tags_lists)
+sorted_tags = [item for item in sorted(tag_counter.items(), key=lambda x: -x[1])]
+sorted_tag_strings = [f"{item[0]} ({item[1]})" for item in sorted_tags]
+
+combs = []
+for tags_per_image in all_tags_lists:
+    for comb in combinations(tags_per_image, 2):
+        combs.append(comb)
+
+most_common_combs = Counter(combs).most_common(20)
+
+def show_images(images):
+    columns = st.columns(3)
+    for idx, img in enumerate(images):
+        col = columns[idx % 3]
+        url = img['url']
+        if url.endswith(".heic"):
+            url = url[:-5] + ".jpg"
+        with col:
+            st.image(url)
+            st.markdown(f"[Links]({url})")
+
+def image_page():
+    show_images(all_images)
+
+def stats_page():
+    pass
+
+if __name__ == "__main__":
+    options = ("Image Gallery", "Image Stats")
+    selection = st.selectbox("Menu", options)
+
+    if selection == "Image Gallery":
+        st.title("Image Gallery")
+        image_page()
+    else:
+        st.title("Image Stats")
+        stats_page()
